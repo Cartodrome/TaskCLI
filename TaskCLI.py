3232588b4dd1b6654ee763c549ffe984475f2f99
Cartodrome <s.h.broster@dunelm.org.uk>
2014-02-26 22:07:18 +0000

    Applied some markups and added unit testing to TaskCLI. App will still need updating

--- TaskCLI.py
+++ TaskCLI.py
@@ -6,11 +6,11 @@ import pickle
 import os
 import sys
 import thread
-
+import argparse
 
 class TaskCLI(cmd.Cmd):
     """CLI that can be used to carry out simple operations."""
-    def __init__(self, tasks={}):
+    def __init__(self, tasks={}, app=None):
         # Initiate the base class.
         cmd.Cmd.__init__(self)
         # Overwrite the prompt with a custom version.
@@ -34,6 +34,9 @@ class TaskCLI(cmd.Cmd):
 
         # Create the object that contains the shortcuts.
         self._shortcut = Shortcut()
+
+        # If a web app is passed then save it off
+        self.app = app
 
     def __getstate__(self):
         self._log("Saving TaskCLI data")
@@ -105,7 +108,7 @@ class TaskCLI(cmd.Cmd):
             return
 
         try:
-            task_name = "-".join([self._current_task.get_name(), task])
+            task_name = "-".join([self._current_task.name, task])
         except:
             task_name = task
 
@@ -147,16 +150,16 @@ class TaskCLI(cmd.Cmd):
             return
 
         # Check that the only running tasks are parents of this task.
-        if self._current_task != task.get_parent():
+        if self._current_task != task.parent:
             self._to_screen("Unable to start this task Either another task is "
                             "already running, this task has a parent task "
                             "which hasn't been started yet or this task is "
                             "already running.")
         else:
             task.start()
-            self._set_new_prompt(text=task.get_name())
+            self._set_new_prompt(text=task.name)
             self._current_task = task
-            self._log("Started Task: %s" % self._current_task.get_name())
+            self._log("Started Task: %s" % self._current_task.name)
             
     def help_starttask(self):
         description = ("Starts an existing task. Once the task is "
@@ -190,14 +193,14 @@ class TaskCLI(cmd.Cmd):
         """
         try:
             self._current_task.stop()
-            self._log("Stopped Task: %s" % self._current_task.get_name())
+            self._log("Stopped Task: %s" % self._current_task.name)
         except:
             self._to_screen("No tasks currently running.")
             return
 
-        self._current_task = self._current_task.get_parent()
+        self._current_task = self._current_task.parent
         try:
-            self._set_new_prompt(text=self._current_task.get_name())
+            self._set_new_prompt(text=self._current_task.name)
         except:
             self._set_new_prompt(text="")
 
@@ -220,7 +223,7 @@ class TaskCLI(cmd.Cmd):
         Returns: Nothing
         """
         # Stop all running tasks and return the CLI to the start before saving.
-        for task in self._get_running_tasks():
+        for task in self.get_tasks(status="Running"):
             task.stop()
         self._current_task = None
         self._set_new_prompt(text="")
@@ -261,7 +264,7 @@ class TaskCLI(cmd.Cmd):
 
         # Get they times for all sub tasks.
         for task in self._tasks.values():
-            task_times[task.get_name()] = self._format_timers(task)
+            task_times[task.name] = self._format_timers(task)
 
         for task, details in sorted(task_times.items(), 
                                     key=lambda x: x[1]):
@@ -286,7 +289,7 @@ class TaskCLI(cmd.Cmd):
         Purpose: Prints a list of all the current tasks.
         """
         for task in self._tasks.values():
-            print task.get_name() 
+            print task.name   
 
     def help_tasks(self):
         description = "Prints a list of all tasks." 
@@ -300,24 +303,25 @@ class TaskCLI(cmd.Cmd):
         arguments = {"command": "The command you require help with"}
         self._help_text(arguments=arguments,
                         description=description)
+
     def _format_timers(self, task):
         """_format_timers
         """
-        title    = "Task: %s" % task.get_name()
+        title    = "Task: %s" % task.name
         headers  = "DATE\t\t START\t STOP\t TOTAL\t STATUS"
         template = "%s\t %s\t %s\t %s\t %s"
         footer   = "TOTAL:\t\t \t \t %s\t %s"        
         task_times = []
         task_times.append(title)
         task_times.append(headers)
-        total_status = task.status()
+        total_status = task.status
         total_time = 0
-        for timer in task._timers:
+        for timer in task.timers:
             date, start = timer.start_time()
             dummy, stop = timer.stop_time()
             time_secs   = timer.total_time().total_seconds()
             time_str    = self._format_seconds(time_secs)
-            status      = timer.status()
+            status      = timer.status
 
             line = template % (date, start, stop, time_str, status)
             task_times.append(line)
@@ -379,9 +383,28 @@ class TaskCLI(cmd.Cmd):
         for line in self._get_wrapper().wrap(message):
             print line
 
-    def _get_running_tasks(self):
-        return [task for task in self._tasks.values() 
-                if task.status() == "Running"]
+    def get_tasks(self,
+                  status=None,
+                  parent=None):
+        filters = []
+
+        if status:
+            filters.append(lambda x: x.status == status)
+
+        if parent:
+            if parent == "None":
+                filters.append(lambda x: x.parent == None)
+            else:
+                filters.append(lambda x: x.parent == parent)
+
+        return self._apply_filters(self._tasks.values(), filters)
+
+    def _apply_filters(self, list_to_be_filtered, list_of_filters):
+        if not list_of_filters:
+            return list_to_be_filtered
+        else:
+            return [elem for elem in list_to_be_filtered if False not in
+                       [test(elem) for test in list_of_filters]]
 
     def _get_logfile(self):
         filename = datetime.datetime.fromtimestamp(
@@ -396,19 +419,11 @@ class TaskCLI(cmd.Cmd):
 class Task():
     """The Task CLI used when Tasks are created."""
     def __init__(self, name, parent):
-        self._name = name
+        self._name   = name
         self._timers = []
-        self._timer = None
+        self._timer  = None
         self._parent = parent
         self._status = "Stopped"
-
-    def get_name(self):
-        """Getter for the Tasks name."""
-        return self._name
-
-    def get_parent(self):
-        """Getter for the Tasks parent."""
-        return self._parent
 
     def start(self):
         """Called to start the task. Kicks off a Timer."""
@@ -428,9 +443,26 @@ class Task():
         except:
             print "Warning: Task %s was not timing!" % self._name
 
+    @property
+    def name(self):
+        """Getter for the Tasks name."""
+        return self._name
+
+    @property
+    def parent(self):
+        """Getter for the Tasks parent."""
+        return self._parent
+
+    @property
     def status(self):
         """Returns the status of the task, either "Running" or "Stopped"."""
         return self._status
+
+    @property
+    def timers(self):
+        """Returns all the tasks Timers."""
+        return self._timers
+
 
 class Timer():
     """Class for timing things."""
@@ -438,7 +470,6 @@ class Timer():
         self._start_time = None
         self._stop_time = None
         self._status = None
-        self.total_time
         self.start()
 
     def start(self):
@@ -485,18 +516,17 @@ class Timer():
             return datetime.datetime.fromtimestamp(time.time()) - \
                    self._start_time
 
+    @property
     def status(self):
-        """Returns the staus of the current timer.
-
-        Returns: The current status: "Stopped" OR "Running"
-        """
+        """ Returns: The current status: "Stopped" OR "Running" """
         return self._status
 
 
 class Shortcut():
     def __init__(self):
         self._const = {
-            "ssh": (self._ssh, "tool for sshing"),
+            "ssh":       (self._ssh, "tool for sshing"),
+            "unit_test": (self._unit_test, "unit testing utility")
         }
 
     def run_cmd(self, text):
@@ -512,6 +542,9 @@ class Shortcut():
     def _ssh(self, args):
         print "ssh, %s" % args
 
+    def _unit_test(self, args):
+        return args
+
     def _summary(self):
         summary = ""
         for sc in self._const:
@@ -522,8 +555,22 @@ class Shortcut():
         return text.split()[0], text.split()[1:]
 
 
-if __name__ == '__main__':
+def get_args():
+    parser = argparse.ArgumentParser(description="TaskCLI")
+    parser.add_argument('mode', metavar='<mode>', type=str, 
+        help="Either 'CLI' (to start tool) or 'UNIT' (to run unit tests).")
+    
+    args = parser.parse_args()
 
+    if args.mode in ["UNIT", "CLI"]:
+        pass
+    else:
+        parser.print_usage()
+        quit()
+
+    return args
+
+def start_cli():
     filename = datetime.datetime.fromtimestamp(
         time.time()).strftime("%d-%m-%Y.p")
     if os.path.isfile(filename):
@@ -535,4 +582,96 @@ if __name__ == '__main__':
         msg = "Welcome to TaskCLI, no data to load."
         cli = TaskCLI()
 
-    cli.cmdloop(msg)
+    cli.cmdloop(msg)    
+
+def start_unit_tests():
+    print "Started Unit Tests\n"
+
+    """ Test Shortcut """
+    print " Testing Shortcut..." 
+    sc = Shortcut()
+    # Test we pass arguments to a function
+    result = sc.run_cmd("unit_test abc 123")
+    assert result == ["abc", "123"]
+    # Test we handle duff shortcuts
+    result = sc.run_cmd("giberish sdf dg sdg ")
+    assert type(result) is str
+    print " ...Shortcut Passed.\n"
+
+    """ Test Timer """
+    print " Testing Timer..."
+    t = Timer()
+    # Check the timer is automatically started.
+    assert t.status == "Running"
+    t.stop()
+    assert t.status == "Stopped"
+    # Check the start and stop times returned.
+    assert type(t.start_time()) is tuple
+    assert type(t.start_time()[0]) is str and type(t.start_time()[1]) is str    
+    assert t.start_time() == t.stop_time()
+    # Check the total seconds is a small number.
+    seconds = t.total_time().total_seconds()
+    assert seconds >= 0 and seconds <= 1
+    print " ...Timer Passed.\n"
+
+    """ Test Task """
+    print " Testing Task..."
+    # Test creation and initiation.
+    parent  = Task(name="parent", parent=None)
+    child1  = Task(name="child1", parent=parent)
+    assert (child1.name == "child1" and child1.parent == parent and
+            child1.status == "Stopped" and child1.timers == [])
+    # Check start and stop behaviour is sensible.
+    child1.stop()
+    assert (child1.status == "Stopped" and child1.timers == [])
+    for ii in range(2):
+        child1.start()
+        assert (child1.status == "Running" and len(child1.timers) == 1 and
+                type(child1.timers[0]) is type(Timer()))
+    child1.stop()
+    assert (child1.status == "Stopped" and len(child1.timers) == 1 and
+                type(child1.timers[0]) is type(Timer()))
+    print " ...Task Passed.\n"
+
+    """ Test TaskCLI """
+    print " Testing TaskCLI..."
+    cli = TaskCLI()
+    cli.help_help()
+    cli.do_SC("dummy")
+    cli.do_addtask("parent_task1")
+    cli.do_addtask("parent_task2")
+    cli.do_addtask("-")
+    cli.do_starttask("parent_task1")
+    cli.do_addtask("child_task1")
+    cli.do_addtask("child_task2")
+    cli.do_starttask("parent_task1-child_task1")
+    cli.do_addtask("child_subtask")
+    cli.do_starttask("parent_task1-child_task1-child_subtask")
+    cli.do_stoptask("")
+    cli.do_stoptask("")
+    cli.do_starttask("parent_task1-child_task2")
+    cli.do_stoptask("")
+    cli.do_addtask("child_task3")
+    cli.do_starttask("parent_task1-child_task3")
+    cli.do_stoptask("")
+    cli.do_stoptask("")
+    cli.do_starttask("parent_task2")
+    cli.do_stoptask("")
+    cli.do_starttask("parent_task1")
+    cli.do_stoptask("")
+    cli.do_tasks("")
+    cli.do_times("")
+    cli.do_exit("")
+    print " ...TaskCLI Passed.\n"
+    print "All Tests Passed."
+
+if __name__ == '__main__':
+
+    args = get_args()
+
+    if args.mode == "CLI":
+        start_cli()
+    elif args.mode == "UNIT":
+        start_unit_tests()
+    else:
+        AssertionError("Failed to start.")
